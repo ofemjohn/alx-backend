174d03e3c510a5cabcd0ca6d90cc977713db76fa
ofemjohn <ofemjohn@gmail.com>
2023-04-17 23:45:07 +0100

    return start and end index after paginating

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 '''
-return in a list for those 
+return in a list for those
 particular pagination parameters.
 '''
 
+
 def index_range(page, page_size):
     '''return a turple '''
-    start_index = (page -1) * page_size
+    start_index = (page - 1) * (page_size)
     ending_index = start_index + page_size
     return (start_index, ending_index)
