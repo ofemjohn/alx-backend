6a22867c591d30e3697e1733294e9ee22a683325
ofemjohn <ofemjohn@gmail.com>
2023-04-20 01:05:02 +0100

    LIFO

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" LIFO Caching """
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ Class that inherits from BaseCaching and is a caching system """
+    def __init__(self):
+        super().__init__()
+        self.last_key = []
+
+    def put(self, key, item):
+        """ Assign to the dictionary, LIFO algorithm, add element """
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(self.last_key))
+            self.cache_data.pop(self.last_key)
+        self.last_key = key
+
+    def get(self, key):
+        """ Return the value linked """
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
+    
