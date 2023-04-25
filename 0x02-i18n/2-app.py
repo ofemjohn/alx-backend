479b40b7a1e38993d6d52d81e1c572aa882315a4
ofemjohn <ofemjohn@gmail.com>
2023-04-26 00:55:48 +0100

    render lanuages

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -29,7 +29,7 @@ def get_locale():
 @app.route('/')
 def index() -> str:
     '''render templates for babel'''
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
