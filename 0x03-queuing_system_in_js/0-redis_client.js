fc253d839f097296369480422422f9bc1ca024b9
Ofem John <ofemjohn@gmail.com>
2023-05-24 08:56:51 +0000

    installed a redis client and checked if its working

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Event handler for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event handler for connection errors
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
